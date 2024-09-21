ae042921c869210303e9f5ef66f561b6b9d94943
Fredaobob <lokgamer01@gmail.com>
2024-09-21 13:02:50 -0300

    feat: small changes

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,13 +1,17 @@
 import GlassyBox from '../components/glassybox';
 import '../styles/about-me.css'
 
+import info from '../assets/images/info-svgrepo-com.svg'
+
 const AboutPage = () => {
   return (  
     <div id='about' className='wrapper-two'>
       <GlassyBox>
         <div className='about-container'>
-          <p>Sobre Mim</p>
+          <h1> > sobre mim_</h1>
+
         </div>
+            <p>18 anos, </p>
       </GlassyBox>
     </div>
   );
