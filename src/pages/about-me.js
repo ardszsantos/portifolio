075111dcf41cb04e2779739af7a47398d51577e3
Fredaobob <lokgamer01@gmail.com>
2024-09-21 13:20:03 -0300

    feat: startedworking on the about-me section, changed the wallpaper

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -4,14 +4,24 @@ import '../styles/about-me.css'
 import info from '../assets/images/info-svgrepo-com.svg'
 
 const AboutPage = () => {
+
+  const greaterthan = '>'
+
   return (  
     <div id='about' className='wrapper-two'>
       <GlassyBox>
         <div className='about-container'>
-          <h1> > sobre mim_</h1>
-
+          <h1> {greaterthan} sobre mim_</h1>
+          <div className='about-p'>
+            <p>Tenho 18 anos, e sou completamente apaixonado por código. Apesar do meu foco atual em <span className='front-span'>front-end</span> , tenho como <br/>
+              objetivo me tornar um desenvolvedor <span className='fullstack-span'>fullstack</span>. Além do web-development, tenho experiência com automatização  <br/>
+              de processos com <span className='python-span'>python</span> e Análise de dados.
+            </p>
+          </div>
+          <div className='about-cards'>
+            <h2>Interesses</h2>
+          </div>
         </div>
-            <p>18 anos, </p>
       </GlassyBox>
     </div>
   );
--- src/pages/hero-page.js
+++ src/pages/hero-page.js
@@ -2,12 +2,15 @@ import '../styles/hero-page.css'
 import GlassyBox from '../components/glassybox';
 
 const HeroSection = () => {
+
+  const greaterthan = '>'
+
   return (  
     <div className="wrapper">
       <GlassyBox>
       <div className='text-container'>
-          <h1><span> > Olá, sou Alexandre <br /> Rodrigues</span> 👋 </h1>
-        <p> > Desenvolvedor com foco em Front End apaixonado<br /> por criar  
+          <h1><span> {greaterthan} Olá, sou Alexandre <br /> Rodrigues</span> 👋 </h1>
+          <p> {greaterthan}  Desenvolvedor com foco em Front End apaixonado<br /> por criar  
           soluções inovadoras e eficientes.</p>
         <button>Saiba Mais</button>
       </div>
