e3b294f29809166bd1d5bd9ec52ad82fe1c2d7ea
Fredaobob <lokgamer01@gmail.com>
2024-09-22 14:18:33 -0300

    somewhat finished the about-me section and colored some text

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,6 +1,8 @@
 
 import '../styles/about-me.css'
 import kubuntu from '../assets/images/kubuntu-svgrepo-com.svg';
+import web from '../assets/images/web-svgrepo-com.svg';
+import ai from '../assets/images/ai-solid-svgrepo-com.svg';
 import CardComponent from '../components/card';
 
 const AboutPage = () => {
@@ -17,14 +19,12 @@ const AboutPage = () => {
               de processos com <span className='python-span'>python</span> e Análise de dados.
             </p>
           </div>
-            <h2>Interesses</h2>
+            <h2 className='interesses'>Interesses</h2>
           <div className='about-cards'>
             <CardComponent img={kubuntu} title={'Linux'} subtitle={`Kubuntu (Ubuntu com KDE nativo) é meu OS preferido para desenvolvimento.  atualmente uso um dualboot na minha máquina pessoal
               com Kubuntu & Windows 10.`}/>
-            <CardComponent img={kubuntu} title={'Linux'} subtitle={`Kubuntu (Ubuntu com KDE nativo) é meu OS preferido para desenvolvimento.  atualmente uso um dualboot na minha máquina pessoal
-              com Kubuntu & Windows 10.`}/>
-            <CardComponent img={kubuntu} title={'Linux'} subtitle={`Kubuntu (Ubuntu com KDE nativo) é meu OS preferido para desenvolvimento.  atualmente uso um dualboot na minha máquina pessoal
-              com Kubuntu & Windows 10.`}/>
+          <CardComponent img={web} title={'Web Development'} subtitle={`Web development é minha princípal área de atuação. Tenho experiência com tecnologias modernas como React, Next.js e Tailwind, e foco em criar interfaces responsivas e dinâmicas.`}/>
+          <CardComponent img={ai} title={'Inteligência Artificial'} subtitle={`Utilizo ferramentas de IA para otimizar e acelerar meu fluxo de trabalho no desenvolvimento web, aproveitando o poder dessas tecnologias para aprimorar a produtividade e a eficiência em meus projetos.`}/>
           </div>
         </div>
     </div>
--- src/pages/hero-page.js
+++ src/pages/hero-page.js
@@ -8,8 +8,8 @@ const HeroSection = () => {
     <div id='hero' className="wrapper">
 
       <div className='text-container'>
-        <h1><span> Olá, sou <br /> {greaterthan} Alexandre  Rodrigues</span> 👋 </h1>
-          <p> Desenvolvedor com foco em Front End apaixonado<br /> por criar  
+        <h1> Olá, sou <br /> <span className='great-span'> {greaterthan}</span> <span className='ale-span'> Alexandre  Rodrigues</span> 👋 </h1>
+          <p> Desenvolvedor Front End apaixonado por criar  
           soluções inovadoras e eficientes.</p>
         <button>Saiba Mais</button>
       </div>
