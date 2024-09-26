f380783aa2fcd61d5be0eb515e5d4047d5af9cf3
Fredaobob <lokgamer01@gmail.com>
2024-09-26 13:55:48 -0300

    worked on the skills section, added some icons and new skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,53 +1,49 @@
 import '../styles/skills.css';
+import { FaDocker, FaAws, FaGit, FaNodeJs, FaReact, FaHtml5, FaCss3, FaJs, FaServer, FaCogs } from 'react-icons/fa';
+import { SiGithubactions, SiNextdotjs, SiTailwindcss, SiGraphql, SiPostgresql, SiMongodb, SiPrisma } from 'react-icons/si';
 
 const SkillsSection = () => {
   const gret = '>'
 
-  return (  
+  return (
     <div id='habilidades' className='wrapper-three'>
       <div id='skills' className='container'>
         <h1> {gret} habilidades_</h1>
-        <div class="skills-desc">
-          <div class="stack1">
-            <h2 className='rotate border-title'>Backend</h2>
-            <div className='skill-list1'>
-              <p>Docker</p>
-              <p>Kubernetes</p>
-              <p>CI/CD Pipelines (GitHub Actions, Jenkins)</p>
-              <p>AWS</p>
-              <p>Nginx</p>
-              <p>Git</p>
-              <p>Version Control (GitHub, GitLab)</p>
-              <p>Server Management</p>
-              <p>Automation</p>
-            </div>
-          </div>
-          <div class="stack2">
-            <h2 className='rotate border-title'>Frontend </h2>
-            <div className='skill-list2'>
-              <p>Docker</p>
-              <p>Kubernetes</p>
-              <p>CI/CD Pipelines (GitHub Actions, Jenkins)</p>
-              <p>AWS</p>
-              <p>Nginx</p>
-              <p>Git</p>
-              <p>Version Control (GitHub, GitLab)</p>
-              <p>Server Management</p>
-              <p>Automation</p>
-            </div>
-          </div>
-          <div class="stack3">
+        <div className="skills-desc">
+          <div className="stack1">
             <h2 className='rotate border-title'>Devops</h2>
-            <div className='skill-list3'>
-              <p>Docker</p>
-              <p>Kubernetes</p>
-              <p>CI/CD Pipelines (GitHub Actions, Jenkins)</p>
-              <p>AWS</p>
-              <p>Nginx</p>
-              <p>Git</p>
-              <p>Version Control (GitHub, GitLab)</p>
-              <p>Server Management</p>
-              <p>Automation</p>
+            <div className='skill-list'>
+              <p><FaDocker /> Docker</p>
+              <p><SiGithubactions /> CI/CD Pipelines (GitHub Actions, Jenkins)</p>
+              <p><FaAws /> AWS</p>
+              <p><FaServer /> Nginx</p>
+              <p><FaGit /> Git</p>
+              <p><FaServer /> Server Management</p>
+              <p><FaCogs /> Automation</p>
+            </div>
+          </div>
+          <div className="stack2">
+            <h2 className='rotate border-title'>Frontend </h2>
+            <div className='skill-list'>
+              <p><FaReact /> React.js</p>
+              <p><SiNextdotjs /> Next.js</p>
+              <p><SiTailwindcss /> Tailwind CSS</p>
+              <p><FaHtml5 /> HTML5</p>
+              <p><FaCss3 /> CSS3</p>
+              <p><FaJs /> JavaScript (ES6+)</p>
+              <p><FaCogs /> Responsive Design</p>
+            </div>
+          </div>
+          <div className="stack3">
+            <h2 className='rotate border-title'>Backend</h2>
+            <div className='skill-list'>
+              <p><FaNodeJs /> Node.js</p>
+              <p><FaCogs /> Express.js</p>
+              <p><FaCogs /> REST APIs</p>
+              <p><SiGraphql /> GraphQL</p>
+              <p><SiPrisma /> Prisma</p>
+              <p><SiPostgresql /> SQL (PostgreSQL, MySQL)</p>
+              <p><SiMongodb /> NoSQL (MongoDB)</p>
             </div>
           </div>
         </div>
