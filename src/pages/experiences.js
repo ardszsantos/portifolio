e7c621e305e0dcab72fcc1dbd0dd7424746c83dd
Fredaobob <lokgamer01@gmail.com>
2024-10-15 11:39:09 -0300

    chore: added the back-prop to each card

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -16,24 +16,36 @@ const ExperiencesSection = (props) => {
             title={t('experiences-senaiTitle')}
             description={t('experiences-senaiDescription')}
             date={t('experiences-senaiDate')}
+            //back
+            backTitle={t('experiences-senaiTitle-back')}
+            backDescription={t('experiences-senaiDescription-back')}
           />
           <ExpCard
             image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVRU8sOl-pLVoZGWBODPTy-56PeVXMd-gglQ&s'}
             title={t('experiences-ebacTitle')}
             description={t('experiences-ebacDescription')}
             date={t('experiences-ebacDate')}
+            //back
+            backTitle={t('experiences-ebacTitle-back')}
+            backDescription={t('experiences-ebacDescription-back')}
           />
           <ExpCard
             image={'https://verpex.com/assets/uploads/images/blog/How-to-become-a-Backend-Developer.jpg?v=1665484477'}
             title={t('experiences-codeTroubleTitle')}
             description={t('experiences-codeTroubleDescription')}
             date={t('experiences-codeTroubleDate')}
+            //back
+            backTitle={t('experiences-codeTroubleTitle-back')}
+            backDescription={t('experiences-codeTroubleDescription-back')}
           />
           <ExpCard
             image={'https://veduca.org/wp-content/uploads/2024/03/Alura-oferece-60-mil-vagas-em-cursos-gratuitos-de-Python-e-analise-de-dados.jpg'}
             title={t('experiences-aluraTitle')}
             description={t('experiences-aluraDescription')}
             date={t('experiences-aluraDate')}
+            //back
+            backTitle={t('experiences-aluraTitle-back')}
+            backDescription={t('experiences-aluraDescription-back')}
           />
         </div>
       </div>
